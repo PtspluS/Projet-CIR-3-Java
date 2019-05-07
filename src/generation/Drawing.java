96379ff849c4216e0516f9a41cf8b5c7a92f530c
Pierricles <pierromanuel5@gmail.com>
2019-05-07 15:55:30 +0200

    Commentaire

--- src/generation/Drawing.java
+++ src/generation/Drawing.java
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 package generation;
 
 import javafx.scene.Group;
@@ -21,14 +11,14 @@ import generation.Voiture;
 
 import java.util.ArrayList;
 
-public class Drawing extends Parent {
+public class Drawing extends Parent {// Classe qui rassemble tout es objets a dessiner
     private int windowweight;
     private int windoheight;
     private double mapweight;
     private double mapheight;
     private Group car;
 
-    Drawing(int windowweight,int windoheight,double mapweight,double mapheight){
+    Drawing(int windowweight,int windoheight,double mapweight,double mapheight){//Constructeur, initialisation des vaviable de taille de fenetre
         this.windoheight=windoheight;
         this.windowweight=windowweight;
         this.mapheight=mapheight;
@@ -36,20 +26,20 @@ public class Drawing extends Parent {
 
     }
 
-    public double scaleX(double tmp){
+    public double scaleX(double tmp){//Une fonction qui met a l'echelle
         return tmp/(this.mapweight)*(this.windowweight-30)+15;
     }
 
-    public double scaleY(double tmp){
+    public double scaleY(double tmp){//Une fonction qui met a l'echelle
         return tmp/(this.mapheight)*(this.windoheight-30)+15;
     }
 
-    public void drawcity(NetWork map){
-        Group city = new Group();
+    public void drawcity(NetWork map){// Fonction qui dessine les ville
+        Group city = new Group();//Cration du groupe de dessin de ville
         this.getChildren().add(city);
         for(City c:map.getCities()){
 
-            Circle cercle = new Circle();
+            Circle cercle = new Circle();//les villes sont des cercles
             cercle.setCenterX(scaleX(c.getX()));
             cercle.setCenterY(scaleY(c.getY()));
             cercle.setRadius(10);
@@ -63,12 +53,12 @@ public class Drawing extends Parent {
 
     }
 
-    public void drawroad(NetWork map){
-        Group road= new Group();
+    public void drawroad(NetWork map){// Dessin des routes
+        Group road= new Group();//groupe de dessins des routes
         this.getChildren().add(road);
         for(Road r:map.getRoads()){
 
-            Line line = new Line();
+            Line line = new Line();//les routes sont des ligne
             line.setStartX(scaleX(r.getStart().getX()));
             line.setStartY(scaleY(r.getStart().getY()));
             line.setEndX(scaleX(r.getEnd().getX()));
@@ -78,17 +68,17 @@ public class Drawing extends Parent {
         }
 
     }
-    public void drawcar(NetWork map){
-        this.car= new Group();
+    public void drawcar(NetWork map){// Fonction de dessin des voitures
+        this.car= new Group();//groupe des voitures
         this.getChildren().add(car);
-        for(Road R:map.getRoads()){
-            double roadangle=Math.atan(-R.getMatrixRepresentation()[1]/R.getMatrixRepresentation()[0]);
+        for(Road R:map.getRoads()){//On parcourt les route
+            double roadangle=Math.atan(-R.getMatrixRepresentation()[1]/R.getMatrixRepresentation()[0]);//calcule angle de la route
 
-            for(ArrayList<Voiture>Voie : R.getVoiesAller()){
+            for(ArrayList<Voiture>Voie : R.getVoiesAller()){//parcourt les voie
 
                 for(Voiture Voit:Voie){
 
-                    Circle cercle = new Circle();
+                    Circle cercle = new Circle();//on dessine un cercle pour chaque voiture
                     cercle.setCenterX(scaleX(Math.cos(roadangle)*Voit.getPositionActuelle()+R.getStart().x));
                     cercle.setCenterY(scaleY(-Math.sin(roadangle)*Voit.getPositionActuelle()+R.getStart().y));
                     cercle.setRadius(4);
--- src/generation/main.java
+++ src/generation/main.java
@@ -28,8 +28,8 @@ public class main extends Application{
     }
 
 
-    public void start(Stage primaryStage) {
-        City lille = new City(0,0,"Lille");
+    public void start(Stage primaryStage) {//Fonction qui gere l'application
+        City lille = new City(0,0,"Lille");//creation des villes
         City tourcoing = new City(50,100,"Tourcoing");
         City paris = new City(40,190,"Paris");
         City marseille = new City(100,5,"Marseille");
@@ -42,32 +42,29 @@ public class main extends Application{
 
 
 
-        primaryStage.setTitle("Network");
-        Group root = new Group();
-        Scene scene = new Scene(root, windowweight, windoheight, Color.LIGHTGREEN);
+        primaryStage.setTitle("Network");//creaton de la fenetre
+        Group root = new Group();//creation du groupe racine
+        Scene scene = new Scene(root, windowweight, windoheight, Color.LIGHTGREEN);//creation de la scene
         primaryStage.setScene(scene);
 
-        Drawing draw=new Drawing(windowweight,windoheight,mapweight ,mapheight);
-        draw.drawroad(map);
-        draw.drawcity(map);
-
-
-
+        Drawing draw=new Drawing(windowweight,windoheight,mapweight ,mapheight);//creation d'un objet dessin
+        draw.drawroad(map);//dessin des routes
+        draw.drawcity(map);//dessin des villes
         root.getChildren().add(draw);
 
-        new AnimationTimer()
+        new AnimationTimer()//gestion de l'animation
         {
             public int delay = 0;
         public void handle(long currentNanoTime)
         {
             if(delay==10) {
                 delay=0;
-                Voiture car = new Voiture(Math.random() * 5 + 10, Math.random() * 30 + 50);
-                map.getRoads().get((int) (Math.round(Math.random() * (map.getRoads().size() - 1)))).debugAjouterAller(car, 0, 0);
+                Voiture car = new Voiture(Math.random() * 5 + 10, Math.random() * 30 + 50);//creation des voitures
+                map.getRoads().get((int) (Math.round(Math.random() * (map.getRoads().size() - 1)))).debugAjouterAller(car, 0, 0);//lancement des voitures
             }else{
                 delay++;
             }
-            for(int j=0;j<map.getRoads().size();j++) {
+            for(int j=0;j<map.getRoads().size();j++) {//boucle de rafraichissement
                 map.getRoads().get(j).avancerFrame(5);
                 draw.removecar();
                 draw.drawcar(map);
